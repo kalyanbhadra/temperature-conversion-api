88ce51b6947e3c433e2d5fbcf42d4b9e896407c2
kalyanbhadra <kalyan.bhadra@gmail.com>
2019-03-24 17:13:43 +0200

    Revert "Changes to deploy the application in External Tomcat"
    This reverts commit e6a8ad7a0b8d0c88e77982d212d47f3da44539a0.

--- src/main/java/com/kals/config/Application.java
+++ src/main/java/com/kals/config/Application.java
@@ -2,8 +2,6 @@ package com.kals.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.kals.controller.TemperatureController;
@@ -12,13 +10,8 @@ import com.kals.controller.TemperatureController;
 @SpringBootApplication 
 //As controller is in different package, this must be explicitly mentioned
 @ComponentScan(basePackageClasses = TemperatureController.class)
-public class Application extends SpringBootServletInitializer {
+public class Application{
   
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
